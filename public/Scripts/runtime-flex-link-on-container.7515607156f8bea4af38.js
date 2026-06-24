63742038d4720ef742a77a56e87ab099c3b5eb38
Bill Turner <billt@turncom360.com>
2026-06-23 22:35:22 -0400

    Replace frontend with uploaded Verge Five layout

--- public/Scripts/runtime-flex-link-on-container.7515607156f8bea4af38.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackJsonpRuntime=self.webpackJsonpRuntime||[]).push([[192],{737(n,e,s){s.r(e),s.d(e,{IGNORE_LINK_ON_CONTAINER_ATTRIBUTE:()=>t.Q,LINK_ON_CONTAINER_ATTRIBUTE:()=>N,init:()=>i});var t=s(8041);const N="data-link-on-container",c=`[${N}]`;async function i(){(0,t.A)({selector:c})}}}]);
